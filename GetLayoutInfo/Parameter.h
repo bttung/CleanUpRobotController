1a9f706c3b8bf45be62006ebfe4560f67a9460e9
bttung <tungfcb@gmail.coom>
2013-12-01 00:32:38 +0900

    メッセージの定義などを修正した

--- GetLayoutInfo/Parameter.h
+++ GetLayoutInfo/Parameter.h
@@ -1,17 +1,35 @@
 // サービス名
 #define LAYOUT_MANAGE_SERVICE_NAME	"LayoutManager"
 
-// メッセージの定義
-// メッセージの定義
-#define	START_MSG				"Start"
-#define	ASK_OBJECT_POS_MSG		"AskObjectPosition"
-#define	ASK_OBSTACLE_POS_MSG	"AskObstaclePosition"
-#define	ASK_ROBOT_POS_MSG		"AskRobotPosition"
-#define	ANS_OBJECT_POS_MSG		"AnswerObjectPosition"
-#define	ANS_OBSTACLE_POS_MSG	"AnswerObstaclePosition"
-#define	ANS_ROBOT_POS_MSG		"AnswerRobotPosition"
-#define	SET_OBJECT_POS_MSG		"SetObjectPosition"
-#define	SET_OBSTACLE_POS_MSG	"SetObstaclePosition"
-#define	SET_ROBOT_POS_MSG		"SetRobotPosition"
-#define	FIN_OBJECT_POS_MSG		"FinishObjectPosition"
-#define	FIN_OBSTACLE_POS_MSG	"FinishObstaclePosition"
+// エンティティのポジションを問い合わせようのメッセージ
+#define	START_ASK_POS_MSG			"StartAskPosition"
+#define	ASK_OBJECT_POS_MSG			"AskObjectPosition"
+#define	ASK_OBSTACLE_POS_MSG		"AskObstaclePosition"
+#define	ASK_ROBOT_POS_MSG			"AskRobotPosition"
+#define	ANS_OBJECT_POS_MSG			"AnswerObjectPosition"
+#define	ANS_OBSTACLE_POS_MSG		"AnswerObstaclePosition"
+#define	ANS_ROBOT_POS_MSG			"AnswerRobotPosition"
+
+// エンティティのポジションを設定するためのメッセージ
+#define START_SET_POSITION_MSG		"StartSetPosition"
+#define	SET_OBJECT_POS_MSG			"SetObjectPosition"
+#define	SET_OBSTACLE_POS_MSG		"SetObstaclePosition"
+#define	SET_ROBOT_POS_MSG			"SetRobotPosition"
+#define	REQUEST_OBJECT_POS_MSG		"RequestObjectPosition"
+#define	REQUEST_OBSTACLE_POS_MSG	"RequestObstaclePosition"
+#define	REQUEST_ROBOT_POS_MSG		"RequestRobotPosition"
+
+// やり取りを終わらせるようのメッセージ
+#define	FIN_ASK_OBJECT_POS_MSG		"FinishAskObjectPosition"
+#define	FIN_ASK_OBSTACLE_POS_MSG	"FinishAskObstaclePosition"
+#define	FIN_SET_OBJECT_POS_MSG		"FinishSetObjectPosition"
+#define	FIN_SET_OBSTACLE_POS_MSG	"FinishSetObstaclePosition"
+#define FIN_ASK_ROBOT_POS_MSG		"FinishAskRobotPosition"
+#define FIN_SET_ROBOT_POS_MSG		"FinishSetRobotPosition"
+
+#define START_SET_POS_MSG			"StartSetPosition"
+#define SET_ENTITY_POS_MSG			"SetEntityPosition"
+#define REQ_ENTITY_POS_MSG			"RequestEntityPosition"
+#define FIN_SET_POS_MSG				"FinishSetPosition"
+
+
