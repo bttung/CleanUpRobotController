eee9e5f5aebcaaeed35caf76142678cd5833b7c7
bttung <tungfcb@gmail.coom>
2013-12-01 00:39:29 +0900

    コメントを英語で書き直した

--- GetLayoutInfo/Parameter.h
+++ GetLayoutInfo/Parameter.h
@@ -1,7 +1,7 @@
-// サービス名
+// Service Name
 #define LAYOUT_MANAGE_SERVICE_NAME	"LayoutManager"
 
-// エンティティのポジションを問い合わせようのメッセージ
+// define Message use when ask Entity Position
 #define	START_ASK_POS_MSG			"StartAskPosition"
 #define	ASK_OBJECT_POS_MSG			"AskObjectPosition"
 #define	ASK_OBSTACLE_POS_MSG		"AskObstaclePosition"
@@ -10,7 +10,7 @@
 #define	ANS_OBSTACLE_POS_MSG		"AnswerObstaclePosition"
 #define	ANS_ROBOT_POS_MSG			"AnswerRobotPosition"
 
-// エンティティのポジションを設定するためのメッセージ
+// define Message use when set Entity Postion
 #define START_SET_POSITION_MSG		"StartSetPosition"
 #define	SET_OBJECT_POS_MSG			"SetObjectPosition"
 #define	SET_OBSTACLE_POS_MSG		"SetObstaclePosition"
@@ -19,7 +19,7 @@
 #define	REQUEST_OBSTACLE_POS_MSG	"RequestObstaclePosition"
 #define	REQUEST_ROBOT_POS_MSG		"RequestRobotPosition"
 
-// やり取りを終わらせるようのメッセージ
+// define Message use when each request finish
 #define	FIN_ASK_OBJECT_POS_MSG		"FinishAskObjectPosition"
 #define	FIN_ASK_OBSTACLE_POS_MSG	"FinishAskObstaclePosition"
 #define	FIN_SET_OBJECT_POS_MSG		"FinishSetObjectPosition"
@@ -27,6 +27,7 @@
 #define FIN_ASK_ROBOT_POS_MSG		"FinishAskRobotPosition"
 #define FIN_SET_ROBOT_POS_MSG		"FinishSetRobotPosition"
 
+// define General Message use for Entity
 #define START_SET_POS_MSG			"StartSetPosition"
 #define SET_ENTITY_POS_MSG			"SetEntityPosition"
 #define REQ_ENTITY_POS_MSG			"RequestEntityPosition"
